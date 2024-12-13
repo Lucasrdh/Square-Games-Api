c550bf7bc911f0ff2f48d29fe4772496645d71e7
Lucas CHRETIEN <lucas.chretien@Digital-Grenoble.local>
2024-12-13 10:45:12 +0100

    initial commit

---
+++ src/main/java/com/squaresgames/Square/games/SquareGamesApplication.java
@@ -0,0 +1,13 @@
+package com.squaresgames.Square.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareGamesApplication.class, args);
+	}
+
+}
